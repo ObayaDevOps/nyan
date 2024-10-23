ea61cb72ec485e10581153074d517323f1fefee0
ObayaDevOps <obayajamesdralega@gmail.com>
2024-10-23 23:00:34 +0200

    readded animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import React from "react";
+import React, { useRef } from "react";
+import { motion } from "framer-motion";
 
-import { 
-  Box, 
-  AbsoluteCenter, 
-  Button,
-  Flex,
-  Icon,
-} from '@chakra-ui/react'
+
+import { Box,AbsoluteCenter, Button,
+Flex,Icon,   ScaleFade,
+
+
+  } from '@chakra-ui/react'
 import { getCloudinaryImage, getCloudinaryImageBlur } from '../util/cloudinaryImageRetreival';
 
 import NextLink from 'next/link'
@@ -44,43 +44,55 @@ export default function Home() {
 
       <Box>
         <AbsoluteCenter mt={{base: 0, md: 0}}>
-          <NextLink href='/#'>
-            <Image
-              src={getCloudinaryImage('nekosero5.png')} 
-              alt="Nekosero Brand Logo"
-              width={250}
-              height={250}
-              priority
-              placeholder="blur"
-              blurDataURL={getCloudinaryImageBlur('nekosero5.png')}
-            />
-          </NextLink>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
+            <NextLink href='/#'>
+              <Image
+                src={getCloudinaryImage('nekosero5.png')} 
+                alt="Nekosero Brand Logo"
+                width={250}
+                height={250}
+                priority
+                placeholder="blur"
+                blurDataURL={getCloudinaryImageBlur('nekosero5.png')}
+              />
+            </NextLink>
+          </motion.div>
 
           <Flex 
             justify='center'
             mt={20}
           >
-            <Button
-              as='a'  
-              _hover={{
-                transform: 'scale(1.15)',
-              }}
-              colorScheme='black' 
-              rounded={'none'} 
-              fontFamily='sidebarFont' 
-              border={'2px'} 
-              variant='outline' 
-              href='/events'
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1, delay: 0.5 }}
             >
-              See What's On
-              <Icon boxSize={4} ml={1} viewBox="0 0 20 20" fill="currentColor">
-                <path
-                  fillRule="evenodd"
-                  d="M6.672 1.911a1 1 0 10-1.932.518l.259.966a1 1 0 001.932-.518l-.26-.966zM2.429 4.74a1 1 0 10-.517 1.932l.966.259a1 1 0 00.517-1.932l-.966-.26zm8.814-.569a1 1 0 00-1.415-1.414l-.707.707a1 1 0 101.415 1.415l.707-.708zm-7.071 7.072l.707-.707A1 1 0 003.465 9.12l-.708.707a1 1 0 001.415 1.415zm3.2-5.171a1 1 0 00-1.3 1.3l4 10a1 1 0 001.823.075l1.38-2.759 3.018 3.02a1 1 0 001.414-1.415l-3.019-3.02 2.76-1.379a1 1 0 00-.076-1.822l-10-4z"
-                  clipRule="evenodd"
-                />
-              </Icon>                
-            </Button>
+              <Button
+                as='a'  
+                _hover={{
+                  transform: 'scale(1.15)',
+                }}
+                colorScheme='black' 
+                rounded={'none'} 
+                fontFamily='sidebarFont' 
+                border={'2px'} 
+                variant='outline' 
+                href='/events'
+              >
+                See What's On
+                <Icon boxSize={4} ml={1} viewBox="0 0 20 20" fill="currentColor">
+                            <path
+                            fillRule="evenodd"
+                            d="M6.672 1.911a1 1 0 10-1.932.518l.259.966a1 1 0 001.932-.518l-.26-.966zM2.429 4.74a1 1 0 10-.517 1.932l.966.259a1 1 0 00.517-1.932l-.966-.26zm8.814-.569a1 1 0 00-1.415-1.414l-.707.707a1 1 0 101.415 1.415l.707-.708zm-7.071 7.072l.707-.707A1 1 0 003.465 9.12l-.708.707a1 1 0 001.415 1.415zm3.2-5.171a1 1 0 00-1.3 1.3l4 10a1 1 0 001.823.075l1.38-2.759 3.018 3.02a1 1 0 001.414-1.415l-3.019-3.02 2.76-1.379a1 1 0 00-.076-1.822l-10-4z"
+                            clipRule="evenodd"
+                            />
+                        </Icon>                
+              </Button>
+            </motion.div>
           </Flex>
         </AbsoluteCenter>
       </Box>
