a8177c9f4df1d6fd4b88fcc8e897eee0f62df250
ObayaDevOps <obayajamesdralega@gmail.com>
2024-12-19 23:33:41 +0300

    reduced time of animation index

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,7 +49,7 @@ export default function Home() {
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ duration: 1 }}
+            transition={{ duration: 0.2 }}
           >
             <NextLink href='/#'>
               <Image
