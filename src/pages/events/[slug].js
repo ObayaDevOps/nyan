630b9be5f16a8736e1e0e0464dca674e94a040d2
ObayaDevOps <obayajamesdralega@gmail.com>
2025-01-16 23:54:45 +0300

    removed h and w og image detail

--- src/pages/events/[slug].js
+++ src/pages/events/[slug].js
@@ -212,8 +212,8 @@ const Event = ({eventPage}) => {
          
           
           <meta property="og:image:type" content="image/png" /> 
-          <meta property="og:image:width" content="120" />
-          <meta property="og:image:height" content="120" />
+          {/* <meta property="og:image:width" content="120" />
+          <meta property="og:image:height" content="120" /> */}
           <meta property="og:url" content="https://www.nekosero.ug/" />
           <meta property="og:type" content="website" />
           
